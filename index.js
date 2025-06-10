0da3f0eb3e88a2beca1736f5b2bb6aa1f1229f5f
Lawrence <lawrenceomozojie@gmail.com>
2025-06-10 20:09:00 +0100

    Improve resonsive layout for medium-sized devices

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close-icon");
 const menu = document.querySelector(".navigation-container");
@@ -36,42 +35,3 @@ document.querySelectorAll(".navigation-container a").forEach((link) => {
     document.body.style.overflow = ""; // Restore scrolling when menu is closed
   });
 });
-=======
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".close-icon");
-const menu = document.querySelector(".navigation-container");
-function updateFooterWidth() {
-  const element = document.querySelector(".home-testimonial");
-  const width = element.getBoundingClientRect().width;
-  document.documentElement.style.setProperty(
-    "--my-element-width",
-    `${width}px`
-  );
-}
-window.addEventListener("resize", updateFooterWidth);
-hamburger.addEventListener("click", (e) => {
-  e.stopPropagation(); // Prevent click from reaching document
-  menu.classList.add("show");
-  document.body.style.overflow = "hidden"; // Prevent scrolling when menu is open
-});
-
-closeIcon.addEventListener("click", () => {
-  menu.classList.remove("show");
-  document.body.style.overflow = ""; // Restore scrolling when menu is closed
-});
-
-menu.addEventListener("click", (e) => {
-  e.stopPropagation(); // Prevent clicks inside the menu from closing it
-});
-
-document.addEventListener("click", () => {
-  menu.classList.remove("show");
-  document.body.style.overflow = ""; // Restore scrolling when menu is closed
-});
-document.querySelectorAll(".navigation-container a").forEach((link) => {
-  link.addEventListener("click", () => {
-    menu.classList.remove("show");
-    document.body.style.overflow = ""; // Restore scrolling when menu is closed
-  });
-});
->>>>>>> d0567c9f8ed169b0f8cf7103c5c966c49efb1e98
